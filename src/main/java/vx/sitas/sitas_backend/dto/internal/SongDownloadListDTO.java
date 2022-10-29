418a0b6de198cf010c55a1de61398e031b81f345
oscarvx00 <oscarvicente@usal.es>
2022-10-29 17:19:10 +0200

    SongDownloadListDTO var rename

--- src/main/java/vx/sitas/sitas_backend/dto/internal/SongDownloadListDTO.java
+++ src/main/java/vx/sitas/sitas_backend/dto/internal/SongDownloadListDTO.java
@@ -5,12 +5,12 @@ public class SongDownloadListDTO {
     private String name;
     private String downloadId;
 
-    private String status;
+    private boolean ready;
 
     public SongDownloadListDTO(SongDownload songDownload){
         this.name = songDownload.getSongName();
         this.downloadId = songDownload.getDownloadId();
-        this.status = songDownload.getStatus();
+        this.ready = songDownload.isStored();
     }
 
     public String getName() {
